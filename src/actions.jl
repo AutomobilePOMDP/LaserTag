2419a2a81c71c58f7eeb2bcc2cf80af8c55e20c7
GuoYu Yang <49673553+pilgrimygy@users.noreply.github.com>
2020-11-02 12:06:29 +0800

    Add files via upload

---
+++ src/actions.jl
@@ -0,0 +1,37 @@
+n_actions(p::LaserTagPOMDP) = p.diag_actions ? 9 : 5
+POMDPs.actions(p::LaserTagPOMDP) = 1:n_actions
+POMDPs.actionindex(p::LaserTagPOMDP, a::Int) = a
+
+const ACTION_NAMES = SVector("north",
+                             "east",
+                             "south",
+                             "west",
+                             "tag",
+                             "northeast",
+                             "southeast",
+                             "southwest",
+                             "northwest")
+
+const TAG_ACTION = 5
+
+const ACTION_DIRS = SVector(Coord(0,1),
+                            Coord(1,0),
+                            Coord(0,-1),
+                            Coord(-1,0),
+                            Coord(0,0),
+                            Coord(1,1),
+                            Coord(1,-1),
+                            Coord(-1,-1),
+                            Coord(-1,1))
+
+const CARDINALS = SVector(Coord(0,1),
+                            Coord(1,0),
+                            Coord(0,-1),
+                            Coord(-1,0))
+
+const DIAGONALS = SVector(Coord(1,1),
+                         Coord(1,-1),
+                         Coord(-1,-1),
+                         Coord(-1,1))
+
+const DIR_TO_ACTION = Dict{Coord, Int}(c=>i for (i,c) in enumerate(ACTION_DIRS))
---
+++ src/heuristics.jl
@@ -0,0 +1,65 @@
+# for fully-observable case
+struct MoveTowards <: Policy end
+
+function action(p::MoveTowards, s::LTState)
+    # try to sneak up diagonally
+    diff = s.opponent-s.robot
+    dx = diff[1]
+    dy = diff[2]
+    if abs(dx) == 1 && abs(dy) == 1
+        return DIR_TO_ACTION[SVector(dx, dy)]
+    else
+        if abs(dx) == 1
+            dx = 0
+        end
+        if abs(dy) == 1
+            dy = 0
+        end
+        return DIR_TO_ACTION[SVector(dx, dy)]
+    end
+end
+
+struct MoveTowardsSampled{RNG} <: Policy
+    rng::RNG
+end
+MoveTowardsSampled() = MoveTowardsSampled(GLOBAL_RNG)
+
+function action(p::MoveTowardsSampled, b)
+    s = rand(p.rng, b)
+    return action(MoveTowards(), s)
+end
+
+
+
+struct OptimalMLSolver <: Solver
+    solver::Solver
+end
+
+struct OptimalML{P<:Policy} <: Policy
+    fo_policy::P 
+end
+
+solve(sol::OptimalMLSolver, p::Union{MDP,POMDP}) = OptimalML(solve(sol.solver, p))
+action(pol::OptimalML, b) = action(pol.fo_policy, mode(b))
+
+struct BestExpectedSolver <: Solver
+    solver::Solver
+end
+
+struct BestExpected{P<:Policy} <: Policy
+    fo_policy::P
+end
+
+solve(sol::BestExpectedSolver, p::Union{MDP,POMDP}) = BestExpected(solve(sol.solver, p))
+function action(pol::BestExpected, b)
+    best_eu = -Inf
+    best_s = first(iterator(b))
+    for s in iterator(b)
+        eu = pdf(b, s)*value(pol.fo_policy, s)
+        if eu > best_eu
+            best_eu = eu
+            best_s = s
+        end
+    end
+    return action(pol.fo_policy, best_s)
+end
